43dcca8fc74caed42ffc29995393c9822c640916
Andrea Dielman <84195166+andreadielman@users.noreply.github.com>
2023-10-30 15:02:59 -0400

    Update common.js

--- common.js
+++ common.js
@@ -1,7 +1,7 @@
 const regexPhoneNumber = /(^|[^\w\d\+])(\+?(?:001|1)[- \.]?)?\(?(\d{3})\)?[- \.]?(\d{3})[- \.]?(\d{4})(?![\w\d])/;
 const regexDomain = /^[\w-]+:\/*\[?([\w\.:-]+)\]?(?::\d+)?/;
 const regexFilter = /{(\d+)}/g;
-const defaultTelFormat = 'tel:+1-{1}-{2}-{3}';
+const defaultTelFormat = '+1{1}{2}{3}';
 const defaultTextFormat = '{0}';
 
 //found this on stack overflow; makes it almost feel like we're in good ol' C#
